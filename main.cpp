5a32025a97b9c427cf0f58025d212ef9939fe81b
Alex Skrypnik <alex@skrypnik.com>
2024-01-20 14:20:56 -0500

    Changes to be committed: 	modified:   .gitignore 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
-int main(int i, const char** args) {
+int main(int number, const char** args) {
     std::cout << "hello " << std::endl;
+
+    for(int i = 0; i < number; ++i)
+    {
+        std::cout << args[i] << std::endl;
+    }
+
+    std::cout << "Program terminated..." << std::endl;
 }
