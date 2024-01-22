69fbea5665aa72f3259021d33b3a3782ff0be4a1
Alex Skrypnik <alex@skrypnik.com>
2024-01-21 19:45:43 -0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,29 @@
 #include <type_traits>
 #include <variant>
 #include <vector>
+#include <map>
 #include "AorB/A.hpp"
 #include "AorB/B.hpp"
+
+template<typename ... T>
+void print(const T& ... v)
+{
+    (std::cout << ... << v);
+    std::cout << std::endl;
+}
 
 using AorB = std::variant<A, B>;
 
 std::vector<AorB> getMyVector() 
 {
     std::vector<AorB> v;
-    std::cout << "Adding A" << std::endl;
+    print("Adding A");
     v.emplace_back(A());
     v.emplace_back(A());
     v.emplace_back(A());
-    std::cout << "Adding B" << std::endl;
+    print("Adding B");
     v.emplace_back(B());
-    std::cout << "Returning the created vector" << std::endl;
+    print("Returning the created vector");
     return v;
 }
 
@@ -30,10 +38,18 @@ void draw(std::vector<AorB>& vector)
 
 int main(int number, const char **args)
 {
+    print("hi", " ", 234, " ", "blum");
+    std::map<int,std::string> m;
+    m[1] = "one";
+    m[2] = "two";
+    for(auto & [a,b] : m)
+    {
+        print(a, " : ", b);
+    }
     std::vector<AorB> v = getMyVector();
-    std::cout << "Constructed vector v" << std::endl;
+    print("Constructed vector v");
     draw(v);
-    std::cout << "Drawing is concluded for v" << std::endl;
+    print("Drawing is concluded for v");
     v.clear();
-    std::cout << "Program terminated..." << std::endl;
+    print("Program terminated...");
 }
